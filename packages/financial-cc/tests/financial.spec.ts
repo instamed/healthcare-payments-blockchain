13077ca7912f7ac95a4df84155468b564afed5ce
instameddevelopment <instamed.development@instamed.com>
2019-03-06 08:27:13 -0600

    initial test
    Signed-off-by: instameddevelopment <instamed.development@instamed.com>

--- packages/financial-cc/tests/financial.spec.ts
+++ packages/financial-cc/tests/financial.spec.ts
@@ -5,18 +5,34 @@ import * as uuid from 'uuid/v4';
 import { MockControllerAdapter } from '@worldsibu/convector-adapter-mock';
 import 'mocha';
 
-import { FinancialControllerClient } from '../client';
+import { FinancialControllerClient, Invoice } from '../client';
+import { InvoiceStatus } from '../src/invoiceStatusEnum';
 
 describe('Fhir', () => {
     // let modelSample: Fhir;
     let adapter: MockControllerAdapter;
-    // let fhirCtrl: FhirControllerClient;
+    let ctrl: FinancialControllerClient;
+    const testingID = 'invoice1';
 
     before(async () => {
+        adapter = new MockControllerAdapter();
+        ctrl = new FinancialControllerClient(adapter);
+        await adapter.init([
+            {
+                version: '*',
+                controller: 'FinancialController',
+                name: join(__dirname, '..')
+            }
+        ]);
+    });
+
+    before(async ()=>{
 
     });
 
-    it('should create a default model', async () => {
-     
+    it('should make a payment', async () => {
+        await ctrl.makePayment(testingID);
+        const invoice = await adapter.getById<Invoice>(testingID);
+        expect(invoice.status).to.be(InvoiceStatus.BALANCED);
     });
 });
