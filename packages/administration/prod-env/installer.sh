443e019a8680554ab3048f40f416bad42c2f46ae
instameddevelopment <instamed.development@instamed.com>
2019-03-11 21:37:21 -0600

    feat: production prep
    Signed-off-by: instameddevelopment <instamed.development@instamed.com>

--- packages/administration/prod-env/installer.sh
+++
@@ -1,7 +0,0 @@
-VIEWS=$(dirname "$0")
-SERVER=34.73.112.77:30042
-CHAINCODE=fhirfinancial
-DB=public_$CHAINCODE
-
-curl -X PUT "http://$SERVER/$DB/_design/$CHAINCODE" \
-  --upload-file $VIEWS/views.json
