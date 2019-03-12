ebc9932a0b20283fe29f5d2271aaed5e46a27ab1
instameddevelopment <instamed.development@instamed.com>
2019-03-12 09:36:06 -0600

    feat: return static html
    Signed-off-by: instameddevelopment <instamed.development@instamed.com>

---
+++ packages/server/src/controllers/default.controller.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from 'express';
+import { getAll } from '../services/account.service';
+import { join } from 'path';
+const router: Router = Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const indexFile = join(__dirname, '../index.html');
+        console.log(indexFile);
+        res.sendFile(indexFile);
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).send(ex);
+    }
+});
+router.get('/healthcheck', async (req: Request, res: Response) => {
+    try {
+        res.send('Success');
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).send(ex);
+    }
+});
+
+export const DefaultCtrl: Router = router;
