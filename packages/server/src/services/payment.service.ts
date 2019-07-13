d3999579b29c13685d54394d475e7640b7da8aba
instameddevelopment <instamed.development@instamed.com>
2019-07-12 23:05:09 -0500

    right name for identities

--- packages/server/src/services/payment.service.ts
+++ packages/server/src/services/payment.service.ts
@@ -2,7 +2,8 @@ import { Init } from '../convectorUtils';
 import { Invoice } from 'financial-cc';
 
 export async function make(invoiceId: string) {
-    const ctrls = await Init('paymentmaker');
+    // Switch to another identity to make payment
+    const ctrls = await Init('provider');
     let invoice = await ctrls.payment.$query().getOneInvoice(invoiceId);
     console.log(invoice);
     await ctrls.payment.$config({ transient: { invoice: new Invoice(invoice).toJSON() } }).make();
